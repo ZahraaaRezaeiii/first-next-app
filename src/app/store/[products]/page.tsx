480423b9dbb82e735f2634521aab7b4bc034992b
Zahra Rezaei <zahraarezaeii@outlook.com>
2025-05-27 18:02:13 +0330

    fix type

--- src/app/store/[products]/page.tsx
+++ src/app/store/[products]/page.tsx
@@ -1,27 +1,20 @@
 import { notFound } from "next/navigation";
 
-interface Props {
-  params: {
-    products: string;
-  };
-}
+type Params = { products: string };
 
-const ProductPage = ({ params }: Props) => {
-  const { products } = params;
-
-  if (products === "2") {
-    return notFound()
+const ProductPage = async ({ params }: { params: Params }) => {
+  if (params.products === "2") {
+    return notFound();
   }
-
-  return <h1>Product: {products}</h1>;
+  return <h1>Product: {params.products}</h1>;
 };
 
 export default ProductPage;
 
 export async function generateStaticParams() {
   return [
-    { products: '1' },
-    { products: '2' },
-    { products: '3' },
+    { products: "1" },
+    { products: "2" },
+    { products: "3" },
   ];
 }
