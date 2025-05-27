d7e91e689067777609c94130e16f4bd5a604dc31
Zahra Rezaei <zahraarezaeii@outlook.com>
2025-05-27 13:46:22 +0330

    add config for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/first-next-app',
 };
 
 export default nextConfig;
