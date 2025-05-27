661827f099ba5326eebf3d4e678cdd629462c3e4
Zahra Rezaei <zahraarezaeii@outlook.com>
2025-05-27 14:06:56 +0330

    changes for build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
   basePath: '/first-next-app',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 };
 
 export default nextConfig;
